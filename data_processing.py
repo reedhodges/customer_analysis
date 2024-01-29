fbff83c8e553b57c852e7c3f0b2c322a40813c65
Reed Hodges <61560859+reedhodges@users.noreply.github.com>
2024-01-29 17:29:44 -0500

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -39,8 +39,7 @@ def process_datetime(df, cols):
         df.drop(col, axis=1, inplace=True)
     return df
 
-
-def clean_data(df, categorical_cols, full_date_cols):
+def clean_and_split_data(filepath, categorical_cols, full_date_cols):
     """
     Perform data cleaning including removing duplicates, replacing categorical outliers, 
     processing datetime columns, and imputing missing values for both categorical and numerical columns.
@@ -53,146 +52,45 @@ def clean_data(df, categorical_cols, full_date_cols):
     Returns:
     DataFrame: The cleaned DataFrame.
     """
+    df = pd.read_csv(filepath, delimiter='\t')
     df = df.drop_duplicates()
     df.drop(['ID'], axis=1, inplace=True)
     df = replace_categorical_outliers(df, categorical_cols, 10)
     df = process_datetime(df, full_date_cols)
-    return df
-
-def impute_categorical_missing_values(df, categorical_cols):
-    """
-    Impute missing values in categorical columns using the most frequent value.
-
-    Parameters:
-    df (DataFrame): The DataFrame to process.
-    categorical_cols (list of str): The list of categorical columns for imputation.
-
-    Returns:
-    DataFrame: The DataFrame with imputed categorical columns.
-    cat_imputer: The fitted imputer object.
-    """
-    cat_imputer = SimpleImputer(strategy='most_frequent').fit(df[categorical_cols])
-    df[categorical_cols] = cat_imputer.transform(df[categorical_cols])
-    return df, cat_imputer
-
-def impute_numerical_missing_values(df, numerical_cols):
-    """
-    Impute missing values in numerical columns using the median value.
-
-    Parameters:
-    df (DataFrame): The DataFrame to process.
-    numerical_cols (list of str): The list of numerical columns for imputation.
-
-    Returns:
-    DataFrame: The DataFrame with imputed numerical columns.
-    num_imputer: The fitted imputer object.
-    """
-    num_imputer = SimpleImputer(strategy='median').fit(df[numerical_cols])
-    df[numerical_cols] = num_imputer.transform(df[numerical_cols])
-    return df, num_imputer
-
-def encode_categorical_variables(df, categorical_cols):
-    """
-    Encode categorical variables using one-hot encoding.
-
-    Parameters:
-    df (DataFrame): The DataFrame to process.
-    categorical_cols (list of str): The list of categorical columns to encode.
-
-    Returns:
-    DataFrame: The DataFrame with encoded categorical variables.
-    encoder: The fitted encoder object.
-    """
-    encoder = OneHotEncoder(sparse_output=False).fit(df[categorical_cols])
-    encoded_data = encoder.transform(df[categorical_cols])
-    encoded_cols = encoder.get_feature_names_out(categorical_cols)
-    encoded_df = pd.DataFrame(encoded_data, columns=encoded_cols)
-    df = pd.concat([df, encoded_df], axis=1)
-    df.drop(categorical_cols, axis=1, inplace=True)
-    return df, encoder, encoded_cols
-
-def remove_outliers(df, cols_to_check, threshold):
-    """
-    Remove outliers from specified columns based on Z-score threshold.
-
-    Parameters:
-    df (DataFrame): The DataFrame to process.
-    cols_to_check (list of str): The list of columns to check for outliers.
-    threshold (float): The Z-score threshold to identify outliers.
-
-    Returns:
-    DataFrame: The DataFrame with outliers removed.
-    """
-    for col in cols_to_check:
-        z_scores = stats.zscore(df[col])
-        abs_z_scores = abs(z_scores)
-        filtered_entries = (abs_z_scores < threshold)
-        df = df[filtered_entries]
-    return df
-
-def scale_data(df, cols_to_scale):
-    """
-    Scale numerical data columns using Min-Max Scaling.
-
-    Parameters:
-    df (DataFrame): The DataFrame to scale.
-    cols_to_scale (list of str): The list of columns to scale.
-
-    Returns:
-    DataFrame: The scaled DataFrame.
-    scaler: The fitted scaler object.
-    """
-    scaler = MinMaxScaler().fit(df[cols_to_scale])
-    df[cols_to_scale] = scaler.transform(df[cols_to_scale])
-    return df, scaler
-
-def preprocess_data(filepath, categorical_cols, full_date_cols, cols_to_check_for_outliers):
-    """
-    Execute the full preprocessing pipeline on the DataFrame.
-
-    Parameters:
-    df (DataFrame): The original DataFrame.
-    categorical_cols (list of str): The list of categorical columns.
-    full_date_cols (list of str): The list of full date columns to process.
-
-    Returns:
-    DataFrame: The fully preprocessed DataFrame.
-    """
-    df = pd.read_csv(filepath, delimiter='\t')
-    df = clean_data(df, categorical_cols, full_date_cols)
-
-    train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
-
-    # save train_df to csv
-    #train_df.to_csv('train.csv', index=False)
-    #train_df = remove_outliers(train_df, cols_to_check_for_outliers, 3)
-
-    train_df, cat_imputer = impute_categorical_missing_values(train_df, categorical_cols)
-    test_df[categorical_cols] = cat_imputer.transform(test_df[categorical_cols])
-
-    train_df, encoder, encoded_cols = encode_categorical_variables(train_df, categorical_cols)
-    encoded_test_data = encoder.transform(test_df[categorical_cols])
-    encoded_test_df = pd.DataFrame(encoded_test_data, columns=encoded_cols, index=test_df.index)
-    test_df = pd.concat([test_df, encoded_test_df], axis=1)
-    test_df.drop(categorical_cols, axis=1, inplace=True)
-
-    numerical_cols = train_df.select_dtypes(include=['number']).columns
-    train_df, num_imputer = impute_numerical_missing_values(train_df, numerical_cols)
-    test_df[numerical_cols] = num_imputer.transform(test_df[numerical_cols])
-
-    train_df, scaler = scale_data(train_df, numerical_cols)
-    test_df[numerical_cols] = scaler.transform(test_df[numerical_cols])
-
+    train_df, test_df = train_test_split(df, test_size=0.2, random_state=0)
     return train_df, test_df
 
+filepath = 'marketing_campaign.csv'
 categorical_cols = ['Education', 'Marital_Status']
 full_date_cols = ['Dt_Customer']
-cols_to_check_for_outliers = ['Income', 'Recency', 'MntWines', 'MntFruits', 'MntMeatProducts', 'MntFishProducts', 'MntSweetProducts', 'MntGoldProds', 'NumDealsPurchases', 'NumWebPurchases', 'NumCatalogPurchases', 'NumStorePurchases', 'NumWebVisitsMonth']
+train_df, test_df = clean_and_split_data(filepath, categorical_cols, full_date_cols)
 
-filepath = 'marketing_campaign.csv'
-train_df, test_df = preprocess_data(filepath, categorical_cols, full_date_cols, cols_to_check_for_outliers)
-print(train_df.shape)
-print(test_df.shape)
-# save to csv
-train_df.to_csv('train.csv', index=False)
-test_df.to_csv('test.csv', index=False)
+boolean_cols = ['AcceptedCmp' + str(i) for i in range(1, 6)] + ['Response', 'Complain']
+numerical_cols = [col for col in train_df.columns if col not in categorical_cols + boolean_cols]
+
+cat_imputer = SimpleImputer(strategy='most_frequent')
+bool_imputer = SimpleImputer(strategy='constant', fill_value=0)
+num_imputer = SimpleImputer(strategy='median')
+
+train_df[categorical_cols] = cat_imputer.fit_transform(train_df[categorical_cols])
+train_df[boolean_cols] = bool_imputer.fit_transform(train_df[boolean_cols])
+train_df[numerical_cols] = num_imputer.fit_transform(train_df[numerical_cols])
+
+test_df[categorical_cols] = cat_imputer.transform(test_df[categorical_cols])
+test_df[boolean_cols] = bool_imputer.transform(test_df[boolean_cols])
+test_df[numerical_cols] = num_imputer.transform(test_df[numerical_cols])
+
+encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+encoded_columns_train = pd.DataFrame(encoder.fit_transform(train_df[categorical_cols]))
+encoded_columns_test = pd.DataFrame(encoder.transform(test_df[categorical_cols]))
+
+# Add encoded columns back to the respective dataframes
+train_df = train_df.join(encoded_columns_train).drop(categorical_cols, axis=1)
+test_df = test_df.join(encoded_columns_test).drop(categorical_cols, axis=1)
+
+scaler = MinMaxScaler()
+train_df[numerical_cols] = scaler.fit_transform(train_df[numerical_cols])
+test_df[numerical_cols] = scaler.transform(test_df[numerical_cols])
+
+#train_df.to_csv('train.csv', index=False)
+#test_df.to_csv('test.csv', index=False)
